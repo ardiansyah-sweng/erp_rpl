640454c5808f735e2a3b992cadb49a082ad26628
ardiansyah-sweng <ardiansyah.2019@outlook.com>
2025-01-07 09:53:02 +0700

    refactor: Memperbaiki penamaan tabel

--- database/migrations/2025_01_06_185213_create_supplier_product_table.php
+++ database/migrations/2025_01_06_185213_create_supplier_product_table.php
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('supplier_supplier_product');
+        Schema::dropIfExists('supplier_product');
     }
 };
