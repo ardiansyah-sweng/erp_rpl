c86511c508ea224833bb95943cc97cc5bdf4f46f
Joangjoo <joangjo22@gmail.com>
2025-03-13 14:26:27 +0700

    Hapus Block Kodingan yang tidak perli d  BranchModel dan Branch Controller - getBranchByID

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -7,15 +7,6 @@ use Illuminate\Http\Request;
 class BranchController extends Controller
 {
     public function getBranchById($id){
-        // $branch = (new Branch)->getBranchById($id);
-        // if(!$branch){
-        //     return response()->json([
-        //         'message' => 'Branch not found'
-        //     ], 404);
-        // }
-        // return response()->json($branch);
-        // dd($branch->toArray());
-
         return (new Branch)->getBranchById($id);
     }
 }
