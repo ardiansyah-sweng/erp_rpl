22b442c572744e776f33652a21a8da2108b4114b
Hansen224 <hansenferdiansyah31@gmail.com>
2025-03-18 16:16:09 +0700

    Update routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\APIProductController;
 use App\Http\Controllers\BranchController;
-use App\Http\Controllers\PurchaseOrderController;
-
 
 Route::get('/', function () {
     return view('dashboard');
@@ -12,16 +10,11 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 });
-Route::get('/supplier/pic/add', function () {
-    return view('supplier/pic/add');
-});
 
-
-
-#API
+# API Routes
 Route::get('/products', [APIProductController::class, 'getProducts']);
 Route::get('/prices', [APIProductController::class, 'getAvgBasePrice']);
-Route::get('/branches/{id}', [App\Http\Controllers\BranchController::class, 'getBranchById']);
+Route::get('/branches/{id}', [BranchController::class, 'getBranchById']);
 
-#Branch
-Route::get('/purchase_orders', [PurchaseOrderController::class, 'getPurchaseOrder'])->name('purchase.orders');
+# Route untuk menambah branch
+Route::post('/branch/add', [BranchController::class, 'addBranch']);
