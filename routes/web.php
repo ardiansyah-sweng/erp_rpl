dc9df23b366e8014e111b5b45edc5d973873d72b
seiryuuuuu <2200018281@webmail.uad.ac.id>
2025-03-18 10:59:24 +0700

    route untuk supplier-pic

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/branches/{id}', [App\Http\Controllers\BranchController::class, 'get
 
 #Branch
 Route::get('/purchase_orders', [PurchaseOrderController::class, 'getPurchaseOrder'])->name('purchase.orders');
+
+#Form Validasi 
+Route::get('/supplier-pic', function () {
+    return view('supplier-pic');
+});
