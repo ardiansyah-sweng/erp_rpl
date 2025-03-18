a8e5b8ef4f1bf535af2999fa954a32cabefc6f77
Hansen224 <hansenferdiansyah31@gmail.com>
2025-03-18 09:11:16 +0700

    Debug commit

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -1,12 +1,68 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Branch;
 use Illuminate\Http\Request;
 
 class BranchController extends Controller
 {
-    public function getBranchById($id){
-        return (new Branch)->getBranchById($id);
+    public function getBranchById($id)
+    {
+        return response()->json([
+            'status' => 'success',
+            'data' => (new Branch)->getBranchById($id)
+        ]);
+    }
+
+    public function addBranch(Request $request)
+    {
+        // Validasi input
+        $request->validate([
+            'branch_name'      => 'required|min:3|unique:branch,branch_name',
+            'branch_address'   => 'required|min:10',
+            'branch_telephone' => 'required|min:10',
+            'branch_status'    => 'required|boolean',
+        ]);
+
+        // Tambahkan data branch
+        $branch = (new Branch)->addBranch($request->all());
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Branch berhasil ditambahkan!',
+            'data' => $branch
+        ]);
+    }
+
+    public function updateBranch(Request $request, $id)
+    {
+        // Validasi input
+        $request->validate([
+            'branch_name'      => 'required|min:3|unique:branch,branch_name,' . $id,
+            'branch_address'   => 'required|min:10',
+            'branch_telephone' => 'required|min:10',
+            'branch_status'    => 'required|boolean',
+        ]);
+
+        // Perbarui data branch
+        $branch = (new Branch)->updateBranch($id, $request->all());
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Branch berhasil diperbarui!',
+            'data' => $branch
+        ]);
+    }
+
+    public function deleteBranch($id)
+    {
+        // Hapus branch berdasarkan ID
+        (new Branch)->deleteBranch($id);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Branch berhasil dihapus!'
+        ]);
     }
 }
--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -16,17 +16,35 @@ class Branch extends Model
     {
         parent::__construct($attributes);
 
-        // Tetapkan nama tabel dan kolom
+        // Tetapkan nama tabel dan kolom sesuai dengan konfigurasi
         $this->table = config('db_constants.table.branch');
         $this->fillable = array_values(config('db_constants.column.branch') ?? []);
     }
 
-    public function getBranchById($id){
+    public function getBranchById($id)
+    {
         return self::where('id', $id)->first();
     }
 
-    public static function getRandomBranchID(){
+    public function addBranch($data)
+    {
+        return self::create($data);
+    }
+
+    public function updateBranch($id, $data)
+    {
+        $branch = self::findOrFail($id);
+        $branch->update($data);
+        return $branch;
+    }
+
+    public function deleteBranch($id)
+    {
+        return self::where('id', $id)->delete();
+    }
+
+    public static function getRandomBranchID()
+    {
         return self::inRandomOrder()->first()->id;
     }
 }
- 
