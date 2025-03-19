d4655fc8639c3e493f976369448030bbcbe2bed2
FarrasAja <ffarrasaja@gmail.com>
2025-03-19 12:03:02 +0700

    merapikan upload image

--- resources/views/supplier/pic/add.blade.php
+++ resources/views/supplier/pic/add.blade.php
@@ -405,20 +405,26 @@
                             <label for="assignment_date" class="form-label">Assignment Date</label>
                             <input type="date" class="form-control" id="assignment_date" name="assignment_date" required>
                         </div>
-                      <div class="d-flex justify-content-between">
+                        <div>
+                            <div><label for="pic_photo" class="form-label">Upload Foto PIC</label></div>
+                                <div class="d-flex justify">
+                                  <div><img id="photo_preview" src={{asset("assets/dist/assets/img/avatar_default.png")}} alt="Avatar Default" class="mt-2" style="max-width: 100px; max-height: 100px;"></div>
+                                  <div>
+                                    <input type="file" class="form-control" id="pic_photo" name="pic_photo" accept="image/*">
+                                  </div>
+                                </div>
+                        </div>
+                        <div class="d-flex justify-content-between">
                             <div>
                                 <div class="mb-3">
                                     <label class="form-check-label" for="status">Status</label>
                                     <input type="checkbox" class="form-check-input" id="status" name="status" value="1">
                                     <label for="status">Aktif</label>
                                 </div>
-                                <button type="button" class="btn btn-primary" onclick="previewData()">Add</button>
-                                <button type="reset" class="btn btn-secondary">Cancel</button>
-                            </div>
-                            <div>
-                                <div><label for="pic_photo" class="form-label">Upload Foto PIC</label></div>
-                                <img id="photo_preview" src={{asset("assets/dist/assets/img/avatar_default.png")}} alt="Avatar Default" class="mt-2" style="max-width: 100px; max-height: 100px;">
-                                <input type="file" class="form-control" id="pic_photo" name="pic_photo" accept="image/*">
+                                <div>
+                                  <button type="button" class="btn btn-primary" onclick="previewData()">Add</button>
+                                  <button type="reset" class="btn btn-secondary">Cancel</button>
+                                </div>
                             </div>
                         </div>
                     </form>
