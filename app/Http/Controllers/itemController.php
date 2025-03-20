f4a5befee2210becccabff0a15e2039b3502d1d8
ardiansyah-sweng <ardiansyah.2019@outlook.com>
2025-03-20 13:09:33 +0700

    refactor:Branch dan Item

--- app/Http/Controllers/itemController.php
+++ app/Http/Controllers/itemController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Item;
 use Illuminate\Http\Request;
 
-class itemController extends Controller
+class ItemController extends Controller
 {
     public function getItemAll(){
         return (new Item)->getItem();
