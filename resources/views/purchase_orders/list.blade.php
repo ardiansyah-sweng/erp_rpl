ffbfdf9d98f89e6192b3625a9a30b926e96b94d7
fdhliakbar <fadhliakbar125@gmail.com>
2025-03-20 10:16:53 +0700

    aa

--- resources/views/purchase_orders/list.blade.php
+++ resources/views/purchase_orders/list.blade.php
@@ -374,7 +374,7 @@
             <div class="row">
               <div class="col-md-12">
                 <div class="card mb-4">
-                  <div class="card-header"><h3 class="card-title">List Table.</h3></div>
+                  <div class="card-header"><h3 class="card-title">List Table</h3></div>
                   <!-- /.card-header -->
                   <div class="card-body">
                   <table class="table table-bordered">
