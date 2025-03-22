2c10aa1964b83b0a907575f7eefda6d14b21f936
FahrezaAja <fahrezaking@gmail.com>
2025-03-23 04:24:23 +0700

    membuat-delete-pada-bagian-supplierPadaModel-dan-SupplierController

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Supplier;
+
 
 class SupplierController extends Controller
 {
-    //
+    public function deleteSupplierByID($id){
+    if (Supplier::where('supplier_id', $id)->delete()) {
+        return redirect()->back()->with('success', 'Supplier deleted successfully');
+        }
+    return redirect()->back()->with('error', 'Supplier not found');
+    }
 }
--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -9,4 +9,9 @@ class Supplier extends Model
     protected $table = 'supplier';
     protected $fillable = ['supplier_id','company_name','address','phone_number','bank_account'];
 
+    public static function deleteSupplier($id)
+    {
+        return self::where('supplier_id', $id)->delete();
+    }
+
 }
