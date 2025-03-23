5a6eb1dca94e8a63a019d1791e5d4c315493ca37
ardiansyah-sweng <ardiansyah.2019@outlook.com>
2025-03-23 15:39:57 +0700

    refactor:item

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
             SupplierSeeder::class,
             BranchSeeder::class,
             PurchaseOrderSeeder::class,
-            GoodsReceiptNoteSeeder::class,
-            BOMSeeder::class
+            // GoodsReceiptNoteSeeder::class,
+            // BOMSeeder::class
             // ProductPriceSeeder::class
         ]);
     }
