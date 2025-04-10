980218977ec4a5ec5fb6ad563caec0ef103edac4
DikaWasHere <andika.pw2634@gmail.com>
2025-04-10 22:53:16 +0700

    add:menambahkan halaman item dan delete untuk item

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\APIProductController;
 use App\Http\Controllers\BranchController;
 use App\Http\Controllers\PurchaseOrderController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ItemController; // tambahkan jika belum
 
 Route::get('/', function () {
     return view('dashboard');
@@ -43,4 +44,6 @@ Route::get('/branch/{id}', [BranchController::class, 'getBranchByID'])->name('br
 Route::get('/purchase_orders/{id}', [PurchaseOrderController::class, 'getPurchaseOrderByID']);
 
 # Items
-Route::get('/items', [App\Http\Controllers\ItemController::class, 'getItemAll']);
+Route::get('/items', [ItemController::class, 'getItemAll']);
+Route::get('/item', [ItemController::class, 'getItemList'])->name('item.list'); // untuk tampilan
+Route::delete('/item/{id}', [ItemController::class, 'deleteItem'])->name('item.delete');
