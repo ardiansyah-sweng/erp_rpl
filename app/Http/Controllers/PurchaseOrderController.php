545ef2b5c73755d27802666e72469aacaa16247d
RicoWidiyatmaaa <129924485+ricoWidiyatma@users.noreply.github.com>
2025-04-10 12:08:44 +0700

    Revert "Rico widiyatma 255 d purchase order controller search purchase order"

--- app/Http/Controllers/PurchaseOrderController.php
+++ app/Http/Controllers/PurchaseOrderController.php
@@ -18,11 +18,5 @@ class PurchaseOrderController extends Controller
         // return view('purchase_orders.detail', compact('purchaseOrder'));
         return response()->json($purchaseOrder);
     }
-    public function searchPurchaseOrder()
-    {
-        $keyword = request()->input('keyword');
-        $purchaseOrders = PurchaseOrder::getPurchaseOrderByKeywords($keyword);
-        return view('purchase_orders.list', compact('purchaseOrders', 'keyword'));
-    }
 
 }
