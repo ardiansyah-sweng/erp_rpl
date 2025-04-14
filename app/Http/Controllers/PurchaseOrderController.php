19d630ac5f2d4fb3879c0142811dd4dbe218c11e
Yoga Rusydi Arifin <yogarifn@gmail.com>
2025-04-14 15:04:26 +0700

    Implement detailed view for Purchase Orders and update routes

--- app/Http/Controllers/PurchaseOrderController.php
+++ app/Http/Controllers/PurchaseOrderController.php
@@ -12,12 +12,22 @@ class PurchaseOrderController extends Controller
         return view('purchase_orders.list', compact('purchaseOrders'));
     }
 
+    // public function getPurchaseOrderByID($po_number)
+    // {
+    //     $purchaseOrder = PurchaseOrder::getPurchaseOrderByID($po_number);
+    //     // return view('purchase_orders.detail', compact('purchaseOrder'));
+    //     return response()->json($purchaseOrder);
+    // }
+
     public function getPurchaseOrderByID($po_number)
     {
-        $purchaseOrder = PurchaseOrder::getPurchaseOrderByID($po_number);
-        // return view('purchase_orders.detail', compact('purchaseOrder'));
-        return response()->json($purchaseOrder);
+        $purchaseOrder = PurchaseOrder::with('supplier', 'details')
+            ->where('po_number', $po_number)
+            ->firstOrFail();
+        
+        return view('purchase_orders.detail', compact('purchaseOrder'));
     }
+    
     public function searchPurchaseOrder()
     {
         $keyword = request()->input('keyword');
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::get('/branch/add', function () {
 Route::get('/supplier/material/add', function () {
     return view('supplier/material/add');
 });
+// Route::get('/purchase_orders/detail/{id}', function () {
+//     return view('purchase_orders/detail');
+// });
+Route::get('/purchase_orders/detail/{id}', [PurchaseOrderController::class, 'getPurchaseOrderByID'])->name('purchase_orders.detail');
 
 # Product 
 Route::get('/product/list', [ProductController::class, 'getProductList'])->name('product.list'); 
