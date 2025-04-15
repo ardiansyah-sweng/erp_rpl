5a62558e6b96497c4346d2102011a430fd73aac0
ferdiansyah-zdn <ferdiansyahzidan81@gmail.com>
2025-04-15 18:33:16 +0700

    input form untuk item

--- resources/views/item/add.blade.php
+++ resources/views/item/add.blade.php
@@ -162,6 +162,7 @@
                 <a href="/dashboard" class="nav-link">
                   <i class="nav-icon bi bi-speedometer"></i>
                   <p>
+                    
                     Dashboard
                   </p>
                 </a>
