169c6785927bd05f2b361ad9132cb7765ea8ff39
ferdiansyah-zdn <ferdiansyahzidan81@gmail.com>
2025-04-17 17:51:28 +0700

    memperbaiki

--- database/seeders/BranchSeeder.php
+++ database/seeders/BranchSeeder.php
@@ -20,11 +20,16 @@ class BranchSeeder extends Seeder
     public function run(): void
     {
         $colBranch = config('db_constants.column.branch');
-        $numOfBranch = $this->faker->numberBetween(1, 10);     
+        $numOfBranch = $this->faker->numberBetween(1, 10); 
 
         for ($i=0; $i<=$numOfBranch; $i++)
         {
             Branch::create([
+                $colBranch['branch_name'] => 'Cabang'.' '.$this->faker->word(),
+                $colBranch['branch_address'] => $this->faker->address(),
+                $colBranch['branch_telephone'] => $this->faker->phoneNumber(),
+                $colBranch['branch_status'] => $this->faker->boolean(),
+  
             ]);
         }
     }
