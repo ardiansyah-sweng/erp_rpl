490733cf0c9324a66275390371e8da80c0d9d20e
wisnuwicaksono233 <nulawrence90@gmail.com>
2025-04-17 19:45:46 +0700

    Update Supplier Material

--- app/Models/SupplierMaterial.php
+++ app/Models/SupplierMaterial.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class SupplierMaterial extends Model
 {
-    public function getSupplierMaterial()
+    public static function getSupplierMaterial()
     {
         return DB::table('supplier_product')->get();
     }
