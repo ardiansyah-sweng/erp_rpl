196dda0959821d0cd3f29b8853640e64ff80b85a
Brigitto23 <brigittoniceforus37@gmail.com>
2025-04-17 23:28:14 +0700

    Brigitto_244D_SupplierPIController_updateSupplierPICDetail_Validation

---
+++ app/Http/Controllers/SupplierPIController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class SupplierPIController extends Controller
+{
+    public function store(Request $request)
+    {
+        // Validasi input data
+        $validator = Validator::make($request->all(), [
+            'name'     => 'required|string|max:255',
+            'email'    => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status'  => 'error',
+                'message' => 'Validasi gagal',
+                'errors'  => $validator->errors(),
+            ], 422);
+        }
+
+        User::create([
+            'name'     => $request->input('name'),
+            'email'    => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return response()->json([
+            'status'  => 'success',
+            'message' => 'Data berhasil disimpan',
+        ], 201);
+    }
+}
