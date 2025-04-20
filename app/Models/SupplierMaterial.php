0be7ba1b7fb27e076c1aadce977c871abf13b787
BambangDimasSaputra123 <138978406+bmbngdims01@users.noreply.github.com>
2025-04-20 20:59:15 +0700

    addSuplierMaterial & InsertTableSupplier

---
+++ app/Models/SupplierMaterial.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SupplierMaterial extends Model
+{
+    protected $table = 'bill_of_material';
+    protected $fillable = [
+        'bom_id',
+        'bom_name',
+        'measurement_unit',
+        'sku',
+        'total_cost',
+        'active',
+        'created_at',
+        'updated_at',
+    ];
+
+}
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ItemController; // tambahkan jika belum
 use App\Http\Controllers\MerkController;
 use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\SupplierMaterialController;
 
 Route::get('/', function () {
     return view('dashboard');
@@ -57,3 +58,7 @@ Route::get('/merk/{id}', [MerkController::class, 'getMerkById']);
 
 #Supplier
 #Route::get('/supplier/{id}', [SupplierController::class, 'getUpdateSupplier']);
+
+#Supplier Material
+Route::get('/supplier-material/create', [SupplierMaterialController::class, 'create'])->name('supplier.material.create');
+Route::post('/supplier-material/store', [SupplierMaterialController::class, 'material'])->name('supplier.material');
