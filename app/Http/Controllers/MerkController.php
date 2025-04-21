77784f66b11de263f4078e0cc9754706b7214f87
198NICHOPRASETYO <2200018198@webmail.uad.ac.id>
2025-04-21 15:02:05 +0700

    memanggil updateMerk($id) dari MerkModel validation dan update ke tabel Merks

--- app/Http/Controllers/MerkController.php
+++ app/Http/Controllers/MerkController.php
@@ -16,23 +16,28 @@ class MerkController extends Controller
         }
 
         return view('merk.detail', compact('merk'));
-         // Validasi input
-         $request->validate([
-            'id' => 'required|integer',
-            'merk' => 'required|string|max:100',
-            ]);
-        
-               // Update data merk
-            $updatedMerk = Merk::getUpdateMerk($request->id, $request->only(['merk']));
-        
-            if (!$updatedMerk) {
-            return response()->json(['message' => 'Data Merk Tidak Tersedia'], 404);
-            }
-        
-            return response()->json([
-            'message' => 'Data Merk berhasil diperbarui',
-            'data' => $updatedMerk,
-            ]);
-        
+  
+    }
+
+    public function getUpdateMerk(Request $request, $id)
+    {
+       // Validasi input
+       $request->validate([
+        'id' => 'required|integer',
+        'merk' => 'required|string|max:100',
+        ]);
+    
+           // Update data merk
+        $updatedMerk = Merk::getUpdateMerk($request->id, $request->only(['merk']));
+    
+        if (!$updatedMerk) {
+        return response()->json(['message' => 'Data Merk Tidak Tersedia'], 404);
+        }
+    
+        return response()->json([
+        'message' => 'Data Merk berhasil diperbarui',
+        'data' => $updatedMerk,
+            ]);
     }
 }
+
