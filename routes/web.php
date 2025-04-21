612bf42f5f2d0344aa225390048397afe89d14cc
179-Rendy-Zulianda <2200018179@webmail.uad.ac.id>
2025-04-21 16:13:36 +0700

    Membuat View merk/add

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,10 @@ Route::get('/purchase_orders/detail/{encrypted_id}', function($encrypted_id) {
 Route::get('/item/add', function () {
     return view('item/add');
 });
+Route::get('/merk/add', function () {
+    return view('merk/add');
+});
+
 
 # Product
 Route::get('/product/list', [ProductController::class, 'getProductList'])->name('product.list');
@@ -66,6 +70,7 @@ Route::post('/item/add', [ItemController::class, 'store'])->name('item.add');
 
 # Merk
 Route::get('/merk/{id}', [MerkController::class, 'getMerkById'])->name('merk.detail');
+Route::post('/merk/add', [MerkController::class, 'addMerk'])->name('merk.add');
 
 #Supplier
 #Route::get('/supplier/{id}', [SupplierController::class, 'getUpdateSupplier']);
