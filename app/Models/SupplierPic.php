dfd15a7e0fc6735a57e6f8202e6f6377cc8a217f
seiryuuuuu <2200018281@webmail.uad.ac.id>
2025-04-22 07:59:52 +0700

    update supplierpic by id

--- app/Models/SupplierPic.php
+++ app/Models/SupplierPic.php
@@ -13,13 +13,19 @@ class SupplierPic extends Model
     {
         parent::__construct($attributes);
 
-        // Tetapkan nama tabel dan kolom
         $this->table = config('db_constants.table.supplier_pic');
         $this->fillable = array_values(config('db_constants.column.supplier_pic') ?? []);
     }
+
+    // method ambil id
     public static function getPICByID($id)
     {
-        return self::find($id); 
+        return self::find($id);
     }
 
+    // jika data tidak ditemukan tidak error
+    public static function getPICOrFailByID($id)
+    {
+        return self::findOrFail($id);
+    }
 }
