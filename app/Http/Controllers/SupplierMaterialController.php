6ff02bc1ae0b1b1858097f027366a8f0df2e59fc
Evalinee <fathurrahmanalghozi.ipa4@gmail.com>
2025-04-22 09:06:17 +0700

    Update validasi supplier material

--- app/Http/Controllers/SupplierMaterialController.php
+++ app/Http/Controllers/SupplierMaterialController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class SupplierMaterialController extends Controller
 {
@@ -13,21 +12,21 @@ class SupplierMaterialController extends Controller
         return view('supplier.material.add');
     }
 
-    // Menyimpan data supplier material ke tabel supplier_product
+    // Validasi data supplier material
     public function addSupplierMaterial(Request $request)
     {
         $validated = $request->validate([
             'product_id'        => 'required|string|size:4',
             'sku'               => 'required|string|max:50',
             'item_name'         => 'required|string|max:50',
-            'measurement_unit'  => 'required|string|max:6',
-            'avg_base_price'    => 'required|integer',
-            'selling_price'     => 'required|integer',
-            'purchase_unit'     => 'required|integer|min:0|max:255',
-            'sell_unit'         => 'required|integer|min:0|max:255',
-            'stock_unit'        => 'required|integer|min:0|max:255',
+            'measurement_unit'  => 'required|string|max:10',
+            'avg_base_price'    => 'required|numeric|min:0',
+            'selling_price'     => 'required|numeric|min:0',
+            'purchase_unit'     => 'required|integer|between:0,255',
+            'sell_unit'         => 'required|integer|between:0,255',
+            'stock_unit'        => 'required|integer|between:0,255',
         ]);
 
-        return redirect()->back()->with('success', 'Data supplier product berhasil ditambahkan!');
+        return redirect()->back()->with('success', 'Data supplier product berhasil divalidasi!');
     }
 }
