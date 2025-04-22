c34583ca3258d8a4837429ff0a17b9697cc02c10
saarasy07 <saarasy07@gmail.com>
2025-04-22 13:19:36 +0700

    SupplierPIController dan SupplierPICModel untuk search by keyword

--- app/Http/Controllers/SupplierMaterialController.php
+++ app/Http/Controllers/SupplierMaterialController.php
@@ -14,8 +14,4 @@ class SupplierMaterialController extends Controller
 
         return view('supplier.material', ['materials' => $materials]);
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> b02cf2841aec6d2466d3724255d19caa37e77a32
