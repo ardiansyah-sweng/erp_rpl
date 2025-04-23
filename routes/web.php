8b87329fd78b83a78609d9b46621679ef9e00849
widyashfya22 <2200018164@webmail.uad.ac.id>
2025-04-23 21:40:34 +0900

    Merge branch 'development' of https://github.com/ardiansyah-sweng/erp_rpl

--- routes/web.php
+++ routes/web.php
@@ -58,6 +58,7 @@ Route::get('/item/add', function () {
 
 # Product
 Route::get('/product/list', [ProductController::class, 'getProductList'])->name('product.list');
+Route::get('/product/detail/{id}', [ProductController::class, 'getProductById'])->name('product.detail');
 
 # API
 Route::get('/products', [APIProductController::class, 'getProducts'])->name('api.products');
