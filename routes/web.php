edf2d220acf124177c85fd5398100c90255a6558
FahrezaAja <fahrezaking@gmail.com>
2025-04-24 17:32:00 +0700

    fahreza246D-mengubah-get-menjadi-delete-pada-routes-delete-supplier

--- routes/web.php
+++ routes/web.php
@@ -95,4 +95,3 @@ Route::get('/supplier/material', [SupplierMaterialController::class, 'getSupplie
 Route::get('/supplier/material', [SupplierMaterialController::class, 'getSupplierMaterial'])->name('supplier.material');
 #Route::get('/supplier/{id}', [SupplierController::class, 'getUpdateSupplier']);
 Route::delete('/delete_supplier/{id}', [SupplierController::class, 'deleteSupplierByID'])->name('supplier.delete');
-#
