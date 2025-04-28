63b0d4a6047234140e0e3c6cba9df8b95720e03e
Evalinee <fathurrahmanalghozi.ipa4@gmail.com>
2025-04-28 18:43:58 +0700

    Fathur_284 Menambahkan validasi supplier material

--- app/Http/Controllers/SupplierMaterialController.php
+++ app/Http/Controllers/SupplierMaterialController.php
@@ -14,4 +14,22 @@ class SupplierMaterialController extends Controller
 
         return view('supplier.material', ['materials' => $materials]);
     }
+
+     // Validasi data supplier material
+     public function addSupplierMaterial(Request $request)
+     {
+         $validated = $request->validate([
+             'product_id'        => 'required|string|size:4',
+             'sku'               => 'required|string|max:50',
+             'item_name'         => 'required|string|max:50',
+             'measurement_unit'  => 'required|string|max:10',
+             'avg_base_price'    => 'required|numeric|min:0',
+             'selling_price'     => 'required|numeric|min:0',
+             'purchase_unit'     => 'required|integer|between:0,255',
+             'sell_unit'         => 'required|integer|between:0,255',
+             'stock_unit'        => 'required|integer|between:0,255',
+         ]);
+ 
+         return redirect()->back()->with('success', 'Data supplier product berhasil divalidasi!');
+     }
 }
