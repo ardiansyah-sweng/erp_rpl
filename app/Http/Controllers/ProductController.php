2a5cf808527245e831b44a10e2940651294d42f9
MRidhoNaufal <2200018201@webmail.uad.ac.id>
2025-04-29 00:58:13 +0700

    Mengubah fungsi store menjadi addProduct di ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -19,7 +19,7 @@ class ProductController extends Controller
         return view('product.detail', compact('product'));
     }
 
-    public function store(Request $request)
+    public function addProduct(Request $request)
     {
         $validatedData = $request->validate([
             'product_id' => 'required|string|unique:products,product_id',
