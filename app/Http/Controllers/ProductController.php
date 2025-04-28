677092709ff9cd612091855793d0f5a07847f1df
MRidhoNaufal <2200018201@webmail.uad.ac.id>
2025-04-28 19:51:45 +0700

    remove insert Table View

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,11 +12,51 @@ class ProductController extends Controller
         return view('product.list', compact('products'));
     }
 
-    public function getProductById($id)
-
+    public function getProductById($id)    
     {
-        $product = Product::with('category')->findOrFail($id);
+        $products = [
+            1 => [
+                'id' => 1,
+                'product_id' => 'KAOS',
+                'product_name' => 'Kaos T-Shirt',
+                'product_type' => 'Finished',
+                'category' => ['category' => 'Pakaian'],
+                'product_description' => 'Kaos T-Shirt',
+                'created_at' => '2025-03-12 19:48:13',
+                'updated_at' => '2025-03-12 19:48:13'
+            ],
+            2 => [
+                'id' => 2,
+                'product_id' => 'TOPI',
+                'product_name' => 'Topi',
+                'product_type' => 'Finished',
+                'category' => ['category' => 'Aksesoris'],
+                'product_description' => 'Topi',
+                'created_at' => '2025-03-12 19:48:13',
+                'updated_at' => '2025-03-12 19:48:13'
+            ],
+            3 => [
+                'id' => 3,
+                'product_id' => 'TASS',
+                'product_name' => 'Tas',
+                'product_type' => 'Finished',
+                'category' => ['category' => 'Aksesoris'],
+                'product_description' => 'Tas',
+                'created_at' => '2025-03-12 19:48:13',
+                'updated_at' => '2025-03-12 19:48:13'
+            ]
+        ];
+    
+        if (!isset($products[$id])) {
+            abort(404, 'Product not found.');
+        }
+    
+        $productData = $products[$id];
+        $productData['category'] = (object)$productData['category'];
+        $product = (object)$productData;
+    
         return view('product.detail', compact('product'));
+
     }
 
     public function create()
@@ -24,57 +64,28 @@ class ProductController extends Controller
         return view('product.add'); 
     }
 
-    public function store(Request $request)
+    public function addProduct(Request $request)
     {
         Product::addProduct($request->all()); 
 
         return redirect()->back()->with('success', 'Produk berhasil ditambahkan.');
     }
 
+    
 
-{
-    $products = [
-        1 => [
-            'id' => 1,
-            'product_id' => 'KAOS',
-            'product_name' => 'Kaos T-Shirt',
-            'product_type' => 'Finished',
-            'category' => ['category' => 'Pakaian'],
-            'product_description' => 'Kaos T-Shirt',
-            'created_at' => '2025-03-12 19:48:13',
-            'updated_at' => '2025-03-12 19:48:13'
-        ],
-        2 => [
-            'id' => 2,
-            'product_id' => 'TOPI',
-            'product_name' => 'Topi',
-            'product_type' => 'Finished',
-            'category' => ['category' => 'Aksesoris'],
-            'product_description' => 'Topi',
-            'created_at' => '2025-03-12 19:48:13',
-            'updated_at' => '2025-03-12 19:48:13'
-        ],
-        3 => [
-            'id' => 3,
-            'product_id' => 'TASS',
-            'product_name' => 'Tas',
-            'product_type' => 'Finished',
-            'category' => ['category' => 'Aksesoris'],
-            'product_description' => 'Tas',
-            'created_at' => '2025-03-12 19:48:13',
-            'updated_at' => '2025-03-12 19:48:13'
-        ]
-    ];
 
-    if (!isset($products[$id])) {
-        abort(404, 'Product not found.');
+
+
+    private function validateProduct(Request $request)
+    {
+    $request->validate([
+        'product_id' => 'required|string|max:10',
+        'product_name' => 'required|string|max:255',
+        'product_type' => 'required|string|max:50',
+        'product_category' => 'required|string|max:50', 
+        'product_description' => 'nullable|string',
+    ]);
     }
 
-    $productData = $products[$id];
-    $productData['category'] = (object)$productData['category'];
-    $product = (object)$productData;
-
-    return view('product.detail', compact('product'));
-}
 
 }
