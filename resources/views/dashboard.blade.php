b249a8a79f25f162bce296087f242e81e3c15ecd
AlfianAzizArdiansyah <alfianardiansyah295@gmail.com>
2025-04-29 18:42:37 +0700

    views/supplier/material/list

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -329,6 +329,12 @@
                     </a>
                   </li>
                   <li class="nav-item">
+                    <a href="/supplier/material/list" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Supplier Material</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
                     <a href="/supplier/add" class="nav-link">
                       <i class="nav-icon bi bi-circle"></i>
                       <p>Tambah Supplier</p>
