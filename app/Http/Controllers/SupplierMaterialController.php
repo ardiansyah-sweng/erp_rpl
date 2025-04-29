f0bf372e7c3fe677e0572099b870531a2749653c
Evalinee <fathurrahmanalghozi.ipa4@gmail.com>
2025-04-29 07:52:14 +0700

    Perbaikan validasi supplier material

--- app/Http/Controllers/SupplierMaterialController.php
+++ app/Http/Controllers/SupplierMaterialController.php
@@ -18,18 +18,15 @@ class SupplierMaterialController extends Controller
      // Validasi data supplier material
      public function addSupplierMaterial(Request $request)
      {
-         $validated = $request->validate([
-             'product_id'        => 'required|string|size:4',
-             'sku'               => 'required|string|max:50',
-             'item_name'         => 'required|string|max:50',
-             'measurement_unit'  => 'required|string|max:10',
-             'avg_base_price'    => 'required|numeric|min:0',
-             'selling_price'     => 'required|numeric|min:0',
-             'purchase_unit'     => 'required|integer|between:0,255',
-             'sell_unit'         => 'required|integer|between:0,255',
-             'stock_unit'        => 'required|integer|between:0,255',
-         ]);
- 
+        $validated = $request->validate([
+            'supplier_id'   => 'required|string|size:6',
+            'company_name'  => 'required|string|max:255', 
+            'product_id'    => 'required|string|max:50',
+            'product_name'  => 'required|string|max:255',
+            'base_price'    => 'required|integer|min:0',
+            'created_at'    => 'nullable|date',
+            'updated_at'    => 'nullable|date',
+        ]);
          return redirect()->back()->with('success', 'Data supplier product berhasil divalidasi!');
      }
 }
