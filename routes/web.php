e8a02437229d97bd058ae52f1c42ce39fdb05e0d
AlfianAzizArdiansyah <alfianardiansyah295@gmail.com>
2025-04-29 22:37:50 +0700

    update routes web

--- routes/web.php
+++ routes/web.php
@@ -99,5 +99,5 @@ Route::get('/merk/{id}', [MerkController::class, 'getMerkById'])->name('merk.det
 Route::post('/merk/add', [MerkController::class, 'addMerk'])->name('merk.add');
 
 #Supplier
-Route::get('/supplier/material/list', [SupplierMaterialController::class, 'getSupplierMaterial'])->name('supplier.material.list');//
+Route::get('/supplier/material/list', [SupplierMaterialController::class, 'getSupplierMaterial'])->name('supplier.material.list');
 Route::post('/supplier/material/add', [SupplierMaterialController::class, 'addSupplierMaterial'])->name('supplier.material.add');
