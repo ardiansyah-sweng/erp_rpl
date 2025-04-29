af0bc583a480d2d15ddb02477f9ccdd8bc395c5e
saarasy07 <saarasy07@gmail.com>
2025-04-29 08:59:59 +0700

    SupplierPIController dan SupplierPICModel untuk SearchSupplier by keywords

---
+++ app/Http/Controllers/SupllierPIController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SupllierPIController extends Controller
+{
+    public function searchSupplierPic(Request $request)
+    {
+        $keywords = $request->input('keywords');
+        $supplierPics = SupplierPICModel::searchSupplierPic($keywords);
+
+        return view('supplier-pic.list', ['supplierPics' => $supplierPics]);
+    }
+
+}
