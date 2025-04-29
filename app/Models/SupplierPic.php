0200558e70f0fe959c03afd53b8d5d1885a2b8c9
niya-mei <2200018242@webmail.uad.ac.id>
2025-04-29 09:05:54 +0700

    Fix SupplierPic.php

--- app/Models/SupplierPic.php
+++ app/Models/SupplierPic.php
@@ -13,8 +13,17 @@ class SupplierPic extends Model
     {
         parent::__construct($attributes);
 
-        
-        $this->table = config('db_constants.table.supplier_pic');
-        $this->fillable = array_values(config('db_constants.column.supplier_pic') ?? []);
+        $this->table = config('db_constants.table.supplier_pic') ?? 'supplier_pic';
+        $this->fillable = array_values(config('db_constants.column.supplier_pic') ?? ['nama', 'email', 'telepon', 'supplier_id']);
+    }
+
+    public static function deleteSupplierPICByID($id)
+    {
+        $pic = self::find($id);
+        if ($pic) {
+            $pic->delete();
+            return true;
+        }
+        return false;
     }
 }
