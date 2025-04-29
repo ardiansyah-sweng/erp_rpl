c32b851cfd8428d0178eafde40cfe533e6c41414
IzzanNurAL <izzannuralmas@gmail.com>
2025-04-29 09:46:31 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -100,7 +100,3 @@ Route::get('/merk/{id}', [MerkController::class, 'getMerkById'])->name('merk.det
 Route::post('/merk/add', [MerkController::class, 'addMerk'])->name('merk.add');
 
 #Supplier
-Route::post('/supplier/material/add', [SupplierMaterialController::class, 'addSupplierMaterial'])->name('supplier.material.add');
-
-# Supplier PIC
-Route::post('/supplier/{supplierID}/add-pic', [SupplierPICController::class, 'addSupplierPIC'])->name('supplier.pic.add');
