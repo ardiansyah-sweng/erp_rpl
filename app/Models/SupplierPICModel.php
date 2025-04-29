efc5a8e4d9080e0ab8d2d8ff793064e499aeb92e
saarasy07 <saarasy07@gmail.com>
2025-04-29 09:17:44 +0700

    SearchSupplier by keywords

---
+++ app/Models/SupplierPICModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SupplierPICModel extends Model
+{
+    protected $table = 'supplier_pic'; // nama tabel sesuai database
+
+    public static function searchSupplierPic($keywords = null)
+    {
+        $query = self::query();
+
+        if ($keywords) {
+            $query->where('supplier_id', 'LIKE', "%{$keywords}%")
+                  ->orWhere('name', 'LIKE', "%{$keywords}%")
+                  ->orWhere('phone_number', 'LIKE', "%{$keywords}%")
+                  ->orWhere('email', 'LIKE', "%{$keywords}%")
+                  ->orWhere('assigned_date', 'LIKE', "%{$keywords}%")
+                  ->orWhere('created_at', 'LIKE', "%{$keywords}%")
+                  ->orWhere('updated_at', 'LIKE', "%{$keywords}%");
+        }
+
+        return $query->orderBy('created_at', 'asc')->paginate(10);
+    }
+
+}
