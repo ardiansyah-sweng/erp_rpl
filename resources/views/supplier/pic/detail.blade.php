b7b1de0185f222e5fb0048df5a91419a0715ac1f
seiryuuuuu <2200018281@webmail.uad.ac.id>
2025-05-02 07:57:04 +0700

    menampilkan preview avatar setelah mengunggah file

--- resources/views/supplier/pic/detail.blade.php
+++ resources/views/supplier/pic/detail.blade.php
@@ -423,7 +423,7 @@
                       $avatarPath = asset("assets/dist/assets/img/{$filename}");
                   @endphp
                 <div class="d-flex gap-3 mb-3 mt-4">
-                    <img src="{{ $avatarPath }}" style="width: 120px;">
+                <img id="photo_preview" src="{{ $avatarPath }}" style="width: 120px;">
                     <input type="file" class="form-control" id="avatar" name="avatar" accept="image/*">
                 </div>
                 <div class="mb-3">
