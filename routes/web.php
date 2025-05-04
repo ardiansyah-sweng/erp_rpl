b626f7da330db2577ef9c4b09c0a2b79dfbffe94
VincentAlfa <vincent111232@gmail.com>
2025-05-04 10:28:39 +0700

    feat: add supplier PIC listing page and pagination functionality

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::get('/supplier/add', function () {
 Route::get('/supplier/detail', function () {
     return view('supplier/detail');
 });
-  
+
 Route::get('/branch/add', function () {
     return view('branch/add');
 });
@@ -82,7 +82,7 @@ Route::get('/branch/{id}', [BranchController::class, 'getBranchByID'])->name('br
 Route::get('/purchase_orders/{id}', [PurchaseOrderController::class, 'getPurchaseOrderByID']);
 Route::get('/purchase-orders/search', [PurchaseOrderController::class, 'searchPurchaseOrder'])->name('purchase_orders.search');
 Route::post('/purchase_orders/add', [PurchaseOrderController::class, 'addPurchaseOrder'])->name('purchase_orders.add'); // tambahan
-Route::get('/purchase_orders/detail/{encrypted_id}', function($encrypted_id) {
+Route::get('/purchase_orders/detail/{encrypted_id}', function ($encrypted_id) {
     $id = EncryptionHelper::decrypt($encrypted_id);
     return app()->make(PurchaseOrderController::class)->getPurchaseOrderByID($id);
 })->name('purchase.orders.detail');
@@ -96,6 +96,10 @@ Route::post('/category/add', [CategoryController::class, 'addCategory'])->name('
 # supplier pic route nya
 Route::get('/supplier/pic/detail/{id}', [SupplierPIController::class, 'getPICByID']);
 Route::put('/supplier/pic/update/{id}', [SupplierPIController::class, 'update'])->name('supplier.pic.update'); //tanbahkan update
+Route::get('/supplier/pic/list', function () {
+    $pics = App\Models\SupplierPic::getSupplierPICAll(10);
+    return view('supplier.pic.list', compact('pics'));
+})->name('supplier.pic.list');
 
 # Items
 Route::get('/items', [ItemController::class, 'getItemAll']);
