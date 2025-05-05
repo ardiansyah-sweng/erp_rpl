eb6ccc8b3c1384f3f3b4f00929e9d265dc1dc004
ferryfebr <2200018261@webmail.uad.ac.id>
2025-05-06 01:41:29 +0700

    mengubah tampilan

--- resources/views/supplier/material/detail.blade.php
+++ resources/views/supplier/material/detail.blade.php
@@ -234,6 +234,13 @@
         <div class="col-sm-6">
           <h3 class="mb-0">Detail Material</h3>
         </div>
+
+        <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-end">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Supplier Material</li>
+                        </ol>
+                    </div>
       </div>
     </div>
   </div>
