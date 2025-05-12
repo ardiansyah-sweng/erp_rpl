59ed1207332aa0180549069950bb25f38e6c4b5e
Faizrizall <rizaladlif@gmail.com>
2025-05-12 16:57:54 +0700

    membuat view/email.blade

--- routes/web.php
+++ routes/web.php
@@ -133,3 +133,5 @@ Route::get('/supplier/detail/{id}', [SupplierController::class, 'getSupplierById
 #Cetak pdf
 Route::get('/category/print', [CategoryController::class, 'printCategoryPDF'])->name('category.print');
 
+#email
+Route::get('/send-po-email/{po_number}', [PurchaseOrderController::class, 'sendPurchaseOrderEmail']);
