086cc13a413f158f53c9ae92982e3caf3997735e
Ardi-Aly <2200018280@webmail.uad.ac.id>
2025-05-14 16:47:53 +0700

    01

--- app/Models/SupplierMaterial.php
+++ app/Models/SupplierMaterial.php
@@ -21,4 +21,11 @@ class SupplierMaterial extends Model
             ->orWhere('product_name', 'like', '%' . $keyword . '%')
             ->get();
     }
+
+    public static function updateSupplierMaterial($id, array $data)
+    {
+        return DB::table('supplier_product')
+            ->where('id', $id)
+            ->update($data);
+    }
 }
