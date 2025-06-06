3726eb7f9e9fe50152938f1d20cf1e88447f4c48
Hansen224 <hansenferdiansyah31@gmail.com>
2025-06-06 23:11:46 +0700

    Hansen Ferdiansyah_C_2200018224 Environtment_WarehouseModel_updateWarehouse($id)

--- app/Models/Warehouse.php
+++ app/Models/Warehouse.php
@@ -13,13 +13,30 @@ class Warehouse extends Model
     {
         parent::__construct($attributes);
 
-        // Tetapkan nama tabel dan kolom
+        // Tetapkan nama tabel dan kolom dari konfigurasi
         $this->table = config('db_constants.table.whouse');
         $this->fillable = array_values(config('db_constants.column.whouse') ?? []);
     }
 
+    /**
+     * Ambil data warehouse berdasarkan ID
+     */
     public function getWarehouseById($id)
     {
         return self::where('id', $id)->first();
+    }
+
+    /**
+     * Update data warehouse berdasarkan ID
+     */
+    public function updateWarehouse($id, $data)
+    {
+        $warehouse = $this->getWarehouseById($id);
+
+        if (!$warehouse) {
+            return false;
+        }
+
+        return $warehouse->update($data);
     }
 }
