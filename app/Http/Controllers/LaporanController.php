06f177064faf202aaea918982a3dc039ae044397
mrtnvzldii <buridayam07@gmail.com>
2025-06-09 20:47:54 +0700

    MartinVizaldi_LaporanCabangPDF

---
+++ app/Http/Controllers/LaporanController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Merk;
+use Barryvdh\DomPDF\Facade\Pdf;
+
+class LaporanController extends Controller
+{
+    public function generateCabangPDF()
+    {
+
+        $data = Merk::getMerkAll();        
+
+        $pdf = PDF::loadView('laporan.cabang', ['data' => $data]);
+        return $pdf->stream('laporan_daftar_cabang.pdf');
+
+    }
+}
