22d92230e9a7334817ee7b650fa8b76910ae9b4c
ardiansyah-sweng <ardiansyah.2019@outlook.com>
2025-06-12 21:17:03 +0700

    fix: add missing 'use erp_rpl;' statement before dropping table

--- database/erp_rpl_assortment_production.sql
+++ database/erp_rpl_assortment_production.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `assortment_production`
 --
-
+use erp_rpl;
 DROP TABLE IF EXISTS `assortment_production`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
