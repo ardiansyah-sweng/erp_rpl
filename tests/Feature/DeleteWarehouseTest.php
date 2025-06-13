71a53e28d303a2e5d15226cac3df916dda58d31a
Harnum221 <2200018221@webmail.uad.ac.id>
2025-06-13 15:11:12 +0700

    Perbaikan pada file test

--- tests/Feature/DeleteWarehouseTest.php
+++ tests/Feature/DeleteWarehouseTest.php
@@ -4,21 +4,24 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\Warehouse;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
 
 class DeleteWarehouseTest extends TestCase
 {
-    use RefreshDatabase;
-
     public function test_delete_warehouse()
     {
-        // Arrange
-        $warehouse = Warehouse::create([
-            'warehouse_name' => 'Gudang Coba',
-            'warehouse_address' => 'Jl. Uji Test 123',
-            'warehouse_telephone' => '081234567890',
-            'is_active' => 1,
-        ]);
+       $warehouse = Warehouse::whereNotIn('id', function ($query) {
+            $query->select('rm_whouse_id')->from('assortment_production')
+                ->union(
+                    DB::table('assortment_production')->select('fg_whouse_id')
+                );
+        })->first();
+
+        // Kalau tidak ada data yang bisa dihapus, skip test
+        if (!$warehouse) {
+            $this->markTestSkipped('Semua warehouse sedang digunakan. Tidak ada yang bisa diuji untuk dihapus.');
+        }
+
 
         // Act
         $response = $this->delete(route('warehouse.delete', ['id' => $warehouse->id]));
