0309aa209b022a3de85788421da6aecf0162d0c6
Rendy Zulianda <2200018179@webmail.uad.ac.id>
2025-06-16 14:29:53 +0700

    getProduction AssortmentProductionModel

---
+++ tests/Feature/AssortProductionModelTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\AssortmentProduction;
+
+class AssortProductionModelTest extends TestCase
+{
+    public function test_get_production_data()
+    {
+        $model = new AssortmentProduction();
+        $data = $model->getProduction();
+        $this->assertNotEmpty($data);
+
+        $sample = $data->take(5);
+
+        echo "\n5 Data dari AssortmentProduction:\n";
+        foreach ($sample as $index => $item) {
+            echo "Data ke-" . ($index + 1) . ": " . json_encode($item->toArray(), JSON_PRETTY_PRINT) . "\n";
+        }
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $data);
+    }
+}
