0d71a179b7ad26db86992171f1b2dc2f894f21a5
padiw123 <fadiyaghifari200@gmail.com>
2025-06-16 15:15:54 +0700

    file test countwarehouse

--- tests/Feature/CountWarehouseControllerTest.php
+++ tests/Feature/CountWarehouseControllerTest.php
@@ -8,20 +8,9 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CountWarehouseControllerTest extends TestCase
 {
-    use RefreshDatabase;
 
     public function test_count_warehouse_controller()
     {
-        for ($i = 0; $i < 65; $i++) {
-            Warehouse::create([
-                'warehouse_name' => 'Gudang ' . $i,
-                'warehouse_address' => 'Jl. Contoh No. ' . $i,
-                'warehouse_telephone' => '08123456789',
-                'is_rm_whouse' => 0,
-                'is_fg_whouse' => 1,
-                'is_active' => 1,
-            ]);
-        }
 
         $response = $this->get('/warehouse/count');
 
