235713631ca2ab4d0a56dd22522e866d8817a317
fdhliakbar <fadhliakbar125@gmail.com>
2025-06-16 14:00:06 +0700

    Update AssortProduction

--- app/Http/Controllers/AssortProductionController.php
+++ app/Http/Controllers/AssortProductionController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
 use App\Models\AssortmentProduction;
 
 class AssortmentProductionController extends Controller
@@ -15,19 +14,6 @@ class AssortmentProductionController extends Controller
             return response()->json(['message' => 'Not found'], 404);
         }
 
-=======
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-
-class AssortProductionController extends Controller
-{
-    public function getProduction()
-    {
-        // Mengambil data dari tabel 'assortment_production' langsung dari query builder
-        $production = DB::table('assortment_production')->get();
-
-        // Kembalikan data dalam format JSON
->>>>>>> 4e5a9c7b00e2bab9707f02dfb0f8ebc8404ecbf8
         return response()->json($production);
     }
 }
