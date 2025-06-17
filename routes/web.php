f1863bfa248472e6e64de0a5af4861b6faba9513
MPuguh <akunbarulol484@gmail.com>
2025-06-17 09:12:51 +0700

    m M_Puguh_D_View

--- routes/web.php
+++ routes/web.php
@@ -116,6 +116,8 @@ Route::get('/supplier/pic/list', function () {
     $pics = App\Models\SupplierPic::getSupplierPICAll(10);
     return view('supplier.pic.list', compact('pics')); //implementasi sementara(menunggu controller dari faiz el fayyed)
 })->name('supplier.pic.list');
+Route::get('/supplier/pic/search', [SupplierPIController::class, 'searchSupplierPic'])->name('supplier.pic.list');
+Route::post('/supplier/{supplierID}/add-pic', [SupplierPIController::class, 'addSupplierPIC'])->name('supplier.pic.add');
 
 Route::get('/supplier-pic', function () {
     $dummySupplierPICs = [
