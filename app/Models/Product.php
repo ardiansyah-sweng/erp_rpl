6403741b1e2152e2de1961f46d8b6de89db753ca
mutiarazahiraaa <2200018175@webmail.uad.ac.id>
2025-06-24 00:17:51 +0700

    menghitung jumlah produk berdasarkan tipe tertentu (RM, HFG, FG)

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -80,11 +80,18 @@ class Product extends Model
         return self::where('product_id', $id)->first();
     }    
 
+    public static function countProductByProductType($shortType)
+    {
+        $colProduct = config('db_constants.column.products');
+
+        return self::where($colProduct['type'], $shortType)->count();
+    }
+
     public static function getProductByType($type)
     {
          return self::where('product_type', $type)->get();
     }
-
+    
     public static function updateProduct($id, array $data)//Sudah sesuai pada ERP RPL
     {
         $product = self::find($id);
