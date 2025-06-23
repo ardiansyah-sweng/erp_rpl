fda2792183454d5e5634d1f84b01d8a5413c84e6
farellbagaskara1 <akunbaruit2@gmail.com>
2025-06-23 13:15:56 +0700

    Membuat fungsi addSupplier

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -37,5 +37,8 @@ class Supplier extends Model
     public static function countSupplier(){
         return self::count();   
     }
-
+    public static function addSupplier($data)
+    {
+        return self::create($data);
+    }
 }
