7ba36c4605d5b2514654d65505378d5aaceaafbb
mrtnvzldii <buridayam07@gmail.com>
2025-06-25 20:14:21 +0700

    UpdateCountSupplierMaterial

--- tests/Feature/CountSupplierMaterialTest.php
+++ tests/Feature/CountSupplierMaterialTest.php
@@ -4,12 +4,10 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\SupplierMaterial;
 
 class CountSupplierMaterialTest extends TestCase
 {
-    use RefreshDatabase;
     public function test_countSupplierMaterialByCategory_does_not_crash()
     {
         if (!Schema::hasTable('supplier_materials')) {
