5aec7355e0363830f06401d8fe2efd1870105922
ZIDANE FERDIANSYAH <138690262+ferdiansyah-zdn@users.noreply.github.com>
2025-07-01 10:27:49 +0700

    Update SupplierMaterialController.php

--- app/Http/Controllers/SupplierMaterialController.php
+++ app/Http/Controllers/SupplierMaterialController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\SupplierMaterial;
-SupplierMaterialController-zidane
 use Illuminate\Support\Facades\DB;
 use Barryvdh\DomPDF\Facade\Pdf;
 
