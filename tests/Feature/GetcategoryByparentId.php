821a3e4975a91bceaa5da37ec9abe86d0d7a0caa
tiyoyowanda <tiyoyowanda72@gmail.com>
2025-07-01 07:35:54 +0700

    Simpan perubahan file GetcategoryByparentId

---
+++ tests/Feature/GetcategoryByparentId.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Category;
+
+class GetcategoryByparentId extends TestCase
+{
+    public function test_model_getCategoryByParent(): void
+    {
+        $parentCategory = Category::inRandomOrder()->first();
+        $this->assertNotNull($parentCategory);
+
+        $childCategories = Category::getCategoryByParent($parentCategory->id);
+
+        $this->assertNotNull($childCategories);
+        foreach ($childCategories as $child) {
+            $this->assertEquals($parentCategory->id, $child->parent_id);
+        }
+    }
+}
