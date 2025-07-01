5c82c7ce81158031e38272f449af742d73a8d44d
FauziRahmatS <uzi.saputro07@gmail.com>
2025-07-01 10:36:07 +0700

    buat file testing nya

--- tests/Unit/ProductTest.php
+++ tests/Unit/ProductTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\Product;
+use App\Models\Item;
 
 
 class ProductTest extends TestCase
@@ -28,4 +29,26 @@ public function test_get_product_by_type()
     }
 }
 
+    /** @test */
+    public function delete_product_ketika_tidak_digunakan_di_items()
+    {
+        //Buat produk dengan product_id khusus
+        $product = Product::create([
+            'product_id' => 'ABC2',
+            'product_name' => 'ABC',
+            'name' => 'Test Product',
+            'product_type' => 'FG',
+            'product_category' => 1,
+            'product_description' => 'hanya nyoba'
+        ]);
+
+        // $product = Product::find(25);
+
+        $this->assertFalse(Item::where('product_id', 'ABC2')->exists());
+
+        $result = Product::deleteProductById($product->id);
+
+        $this->assertTrue($result);
+        $this->assertDatabaseMissing('products', ['id' => $product->id]);
+    }
 }
