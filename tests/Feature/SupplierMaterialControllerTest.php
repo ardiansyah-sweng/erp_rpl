04bc849503cb5d7676bc34d461483980b35be36a
ferdiansyah-zdn <ferdiansyahzidan81@gmail.com>
2025-07-02 19:17:25 +0700

    getSupplierMaterialByProductType - 166

--- tests/Feature/SupplierMaterialControllerTest.php
+++ tests/Feature/SupplierMaterialControllerTest.php
@@ -9,24 +9,6 @@ class SupplierMaterialControllerTest extends TestCase
 {
     public function test_returns_supplier_materials_by_product_type()
     {
-        // Setup: Insert dummy product
-        DB::table('products')->insert([
-            'product_id' => 'P001',
-            'product_name' => 'Bahan Mentah A',
-            'product_type' => 'RM',
-            'product_category' => '1',
-            'product_description' => 'Deskripsi untuk bahan mentah A.'
-        ]);
-
-        // Setup: Insert dummy supplier product
-        DB::table('supplier_product')->insert([
-            'supplier_id' => 'SPL001',
-            'company_name' => 'PT Contoh Supplier',
-            'product_id' => 'P001',
-            'product_name' => 'Bahan Mentah A',
-            'base_price' => 50000
-        ]);
-
         // Kirim request dengan 2 parameter: supplier_id dan product_type
         $response = $this->get('/supplier-material/SPL001/RM');
 
