f2ba31994a291206b648225dcf2afb8b390b6669
FauziRahmatS <uzi.saputro07@gmail.com>
2025-07-07 14:55:45 +0700

    Membuat 2 TestFunction

--- tests/Unit/ProductTest.php
+++ tests/Unit/ProductTest.php
@@ -30,15 +30,38 @@ public function test_get_product_by_type()
 }
 
     /** @test */
-    public function delete_product_ketika_tidak_digunakan_di_items()
+    public function delete_product_ketika_digunakan_di_items()
     {
-        $product = Product::find(5);
+        $product = Product::find(9);
 
-        $this->assertFalse(Item::where('product_id', $product->product_id)->exists());
+        $this->assertTrue(Item::where('product_id', $product->product_id)->exists());
 
         $result = Product::deleteProductById($product->id);
 
-        $this->assertTrue($result);
-        $this->assertDatabaseMissing('products', ['id' => $product->id]);
+        $this->assertFalse($result);
+        $this->assertDatabaseHas('products', ['id' => $product->id]);
+    }
+    
+    /** @test */
+    public function delete_product_ketika_tidak_digunakan_di_items()
+    {
+    // Arrange: Buat produk baru di database
+    $product = Product::create([
+        'product_id' => 'ABC2',
+        'product_name' => 'ABC',
+        'name' => 'Test Product',
+        'product_type' => 'FG',
+        'product_category' => 1,
+        'product_description' => 'hanya nyoba'
+    ]);
+
+    // Pastikan produk berhasil masuk ke database
+    $this->assertDatabaseHas('products', ['id' => $product->id]);
+
+    $this->assertFalse(Item::where('product_id', $product->product_id)->exists());
+
+    $result = Product::deleteProductById($product->id);
+    $this->assertTrue($result);
+    $this->assertDatabaseMissing('products', ['id' => $product->id]);
     }
 }
