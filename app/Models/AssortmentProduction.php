06bd7dec5261da1959b22401b85ba4ed2cc97626
syaaamhrn <2200018209@webmail.uad.ac.id>
2025-07-07 13:34:05 +0700

    AssortmentProductionSearch_209

--- app/Models/AssortmentProduction.php
+++ app/Models/AssortmentProduction.php
@@ -17,4 +17,28 @@ class AssortmentProduction extends Model
         $this->table = config('db_constants.table.assort_prod');
         $this->fillable = array_values(config('db_constants.column.assort_prod') ?? []);
     }
+
+    public static function SearchOfAssortmentProduction($keywords = null)
+    {
+        $query = self::query();
+
+        if ($keywords) {
+            $query->where('id', 'LIKE', "%{$keywords}%")
+                  ->orWhere('in_production', 'LIKE', "%{$keywords}%")
+                  ->orWhere('production_number', 'LIKE', "%{$keywords}%")
+                  ->orWhere('sku', 'LIKE', "%{$keywords}%")
+                  ->orWhere('branch_id', 'LIKE', "%{$keywords}%")
+                  ->orWhere('rm_whouse_id', 'LIKE', "%{$keywords}%")
+                  ->orWhere('fg_whouse_id', 'LIKE', "%{$keywords}%")
+                  ->orWhere('production_date', 'LIKE', "%{$keywords}%")
+                  ->orWhere('finished_date', 'LIKE', "%{$keywords}%")
+                  ->orWhere('description', 'LIKE', "%{$keywords}%")
+                  ->orWhere('created_at', 'LIKE', "%{$keywords}%");
+        }
+
+        return $query->orderBy('created_at', 'asc')->paginate(10);
+    }
+
 }
+
+
