6846bb67f5df631775f92db100b885ccbe267dbd
saarasy07 <saarasy07@gmail.com>
2025-07-08 08:40:13 +0700

    SearchOfBillMaterial

--- app/Models/BillOfMaterial.php
+++ app/Models/BillOfMaterial.php
@@ -41,4 +41,22 @@ class BillOfMaterial extends Model
     {
         return self::orderBy('created_at', 'asc')->paginate(10);
     }
+
+    public static function SearchOfBillMaterial($keywords = null)
+    {
+        $query = self::query();
+
+        if ($keywords) {
+            $query->where('bom_id', 'LIKE', "%{$keywords}%")
+                  ->orWhere('bom_name', 'LIKE', "%{$keywords}%")
+                  ->orWhere('measurement_unit', 'LIKE', "%{$keywords}%")
+                  ->orWhere('total_cost', 'LIKE', "%{$keywords}%")
+                  ->orWhere('active', 'LIKE', "%{$keywords}%")
+                  ->orWhere('created_at', 'LIKE', "%{$keywords}%")
+                  ->orWhere('updated_at', 'LIKE', "%{$keywords}%");
+        }
+
+        return $query->orderBy('created_at', 'asc')->paginate(10);
+    }
+
 }
