15a97f19272fcc1c23100a6ea576fc5c87388f58
Harnum221 <2200018221@webmail.uad.ac.id>
2025-07-18 18:38:12 +0700

    Perbaikan

--- app/Http/Controllers/WarehouseController.php
+++ app/Http/Controllers/WarehouseController.php
@@ -48,7 +48,7 @@ class WarehouseController extends Controller
 
     public function deleteWarehouse($id)
     {
-        $warehouse = Warehouse::find($id);
+        $warehouse = Warehouse::getWarehouseById($id);
 
         if (!$warehouse) {
             return response()->json([
