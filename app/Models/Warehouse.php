80819f18b0d998bbf31194832da7ad565e04cee9
farellbagaskara1 <akunbaruit2@gmail.com>
2025-07-24 19:02:12 +0700

    membuat getWarehouseAll dan file test

--- app/Models/Warehouse.php
+++ app/Models/Warehouse.php
@@ -28,7 +28,7 @@ class Warehouse extends Model
         return self::count();
     }
 
-     public function updateWarehouse($id, $data)
+    public function updateWarehouse($id, $data)
     {
         $warehouse = $this->getWarehouseById($id);
 
@@ -99,4 +99,9 @@ class Warehouse extends Model
             'is_active' => $data['is_active'],
         ]);
     }
+    public static function getWarehouseAll()
+    {
+        return self::orderBy('created_at', 'asc')->paginate(10);
+    }
+
 }
