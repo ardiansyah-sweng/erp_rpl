6cc94d74891c1e2a56a051f030d74bc51ffe0f04
FajarHidayat25 <fajarhdy005@gmail.com>
2025-07-25 08:31:36 +0700

    Menampilkan kolom in_production antara Aktif atau NonAktif

--- resources/views/assortment_production/list.blade.php
+++ resources/views/assortment_production/list.blade.php
@@ -372,7 +372,7 @@
                     <!--begin::Row-->
                     <div class="row align-items-center">
                         <div class="col-sm-6 d-flex align-items-center">
-                            <h3 class="mb-0 me-2">produk</h3> 
+                            <h3 class="mb-0 me-2">Productions</h3> 
                             <a href="# " class="btn btn-primary btn-sm">Tambah</a>
                         </div>
                         <div class="col-sm-6">
@@ -446,9 +446,9 @@
                                 <td>{{ $produksi->finished_date ? "FInish" : "Null"}}</td>
                                 <td>
                                     @if($produksi->in_production == 1)
-                                        <span class="badge bg-success">Aktif</span>
+                                        <span class="badge bg-success">YES</span>
                                     @else
-                                        <span class="badge bg-secondary">NonAktif</span>
+                                        <span class="badge bg-danger">NO</span>
                                     @endif
                                 </td>
                                 <td>{{ $produksi->description}}</td>
