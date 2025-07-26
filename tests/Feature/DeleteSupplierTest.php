07c9324bb02357d2c24aca85bb3cd935b3260fbd
singgih <2200018200@webmail.uad.ac.id>
2025-07-26 20:21:45 +0700

    1. Bisa dihapus kalau supplier belum pernah diberi PO, 2. Tidak bisa dihapus kalau sudah pernah ada PO"

--- tests/Feature/DeleteSupplierTest.php
+++ tests/Feature/DeleteSupplierTest.php
@@ -7,22 +7,19 @@ use App\Models\Supplier;
 
 class DeleteSupplierTest extends TestCase
 {
-    public function test_can_delete_supplier_by_id()
+    public function test_cannot_delete_supplier_by_id()
     {
-        $supplierId = 'SUP002';
+        $supplierId = 'SUP003';
 
         $supplier = Supplier::where('supplier_id', $supplierId)->first();
         $this->assertNotNull($supplier, 'Supplier tidak ditemukan di database.');
 
         $result = Supplier::deleteSupplier($supplierId);
 
-        $this->assertTrue($result['success']);
-        $this->assertEquals('Supplier berhasil dihapus.', $result['message']);
-
-        $this->assertDatabaseMissing('supplier', [
-            'supplier_id' => $supplierId
-        ]);
+        $this->assertFalse($result['success']);
+        $this->assertEquals('Supplier tidak dapat dihapus karena sudah memiliki PO.', $result['message']);
     }
+
 
 
 }
