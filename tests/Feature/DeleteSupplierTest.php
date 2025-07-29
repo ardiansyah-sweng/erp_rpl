7776dd085ea96851b031cb3a962221eeecaac832
singgih <2200018200@webmail.uad.ac.id>
2025-07-29 22:21:20 +0700

    menambahkan satu method test lagi untuk bisa sukses menghapus supplier

--- tests/Feature/DeleteSupplierTest.php
+++ tests/Feature/DeleteSupplierTest.php
@@ -7,7 +7,25 @@ use App\Models\Supplier;
 
 class DeleteSupplierTest extends TestCase
 {
-    public function test_cannot_delete_supplier_by_id()
+     public function test_can_delete_supplier_by_id()
+    {
+        $supplierId = 'SUP003';
+
+        $supplier = Supplier::where('supplier_id', $supplierId)->first();
+        $this->assertNotNull($supplier, 'Supplier tidak ditemukan di database.');
+
+        $result = Supplier::deleteSupplier($supplierId);
+        //dd($result);
+
+        $this->assertTrue($result['success']);
+        $this->assertEquals('Supplier berhasil dihapus.', $result['message']);
+
+        $this->assertDatabaseMissing('suppliers', [
+            'supplier_id' => $supplierId
+        ]);
+    }
+
+    public function test_cannot_delete_supplier_with_po()
     {
         $supplierId = 'SUP003';
 
