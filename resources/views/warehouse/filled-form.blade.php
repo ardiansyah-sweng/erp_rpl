65480002ee7dc6314634afc4f41813d573b858aa
ricoprima03 <ricoprima03@gmail.com>
2025-07-29 15:20:31 +0700

    konflik

--- resources/views/warehouse/filled-form.blade.php
+++ resources/views/warehouse/filled-form.blade.php
@@ -756,7 +756,7 @@
       crossorigin="anonymous"
     ></script>
     <!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
-    <script src="../../dist/js/adminlte.js"></script>
+    <!-- <script src="../../dist/js/adminlte.js"></script> -->
     <!--end::Required Plugin(AdminLTE)--><!--begin::OverlayScrollbars Configure-->
     <script>
       const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
@@ -913,6 +913,5 @@
       });
     });
   </script>
-    </script>
   </body>
 </html>
