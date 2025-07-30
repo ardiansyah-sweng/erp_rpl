941fa7c0350cc909aeaf093babdd3128df09e2ba
tiyoyowanda <tiyoyowanda72@gmail.com>
2025-07-30 14:39:39 +0700

    cetak pdf seluruh supplier PIC berdasarkan Supplier ID

---
+++ tests/Feature/SupplierPdfTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SupplierPdfTest extends TestCase
+{
+    /**
+     * Test generate PDF by supplier ID (real data in DB)
+     */
+    public function testGeneratePdfBySupplierId()
+    {
+        $supplierId = 'SUP001';
+
+        $response = $this->get('/supplier-pic/pdf/' . $supplierId);
+
+        $response->assertStatus(200);
+        $response->assertHeader('Content-Type', 'application/pdf');
+    }
+}
