230f83432f5d3af978916e3905f14c32b890563c
iqvalarbani <iqbalarbani7676@gmail.com>
2025-07-30 21:49:15 +0700

    Membuat query untuk mendapatkan seluruh warehouse

--- app/Models/Warehouse.php
+++ app/Models/Warehouse.php
@@ -99,4 +99,8 @@ class Warehouse extends Model
             'is_active' => $data['is_active'],
         ]);
     }
+            public function getWareHouseAll()
+    {
+        return self::all();
+    }
 }
