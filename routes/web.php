ee05dd3dd86096e109f8f385d57a5dc4b2e419f6
ricoprima03 <ricoprima03@gmail.com>
2025-08-01 10:08:02 +0700

    view  1

--- routes/web.php
+++ routes/web.php
@@ -151,7 +151,7 @@ Route::post('/purchase-orders/pdf', [PurchaseOrderController::class, 'generatePu
 Route::get('/purchase_orders', [PurchaseOrderController::class, 'getPurchaseOrder'])->name('purchase.orders');
 Route::get('/purchase-order/status/{status}', [PurchaseOrderController::class, 'getPurchaseOrderByStatus']);
 Route::post('/purchase-orders/send-email', [App\Http\Controllers\PurchaseOrderController::class, 'sendMailPurchaseOrder'])->name('purchase_orders.send_email');
-
+Route::get('/purchase-orders/{id}/print', [PurchaseOrderController::class, 'printPurchaseOrderToPDF'])->name('purchase_orders.print');
 # supplier pic route nya
 Route::get('/supplier/pic/detail/{id}', [SupplierPIController::class, 'getPICByID']);
 Route::put('/supplier/pic/update/{id}', [SupplierPIController::class, 'update'])->name('supplier.pic.update'); //tanbahkan update
