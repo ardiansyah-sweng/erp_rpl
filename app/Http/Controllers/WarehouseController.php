6e1c936ea7bd865a307d38d916ada7b6fdbcf47d
ricoprima03 <ricoprima03@gmail.com>
2025-08-14 09:21:28 +0700

    perbaikan  mengembalikan yang  terhapus

--- app/Http/Controllers/WarehouseController.php
+++ app/Http/Controllers/WarehouseController.php
@@ -19,6 +19,7 @@ class WarehouseController extends Controller
         }
 
         return view('warehouse.detail', compact('warehouse'));
+
     }
 
     public function countWarehouse()
@@ -52,7 +53,35 @@ class WarehouseController extends Controller
 
     public function exportPdf()
     {
-        $warehouse = Warehouse::getWarehouseAll();
+        $warehouse = [
+            [
+                'id' => 1,
+                'warehouse_name' => 'Warehouse A',
+                'warehouse_address' => 'Location A',
+                'warehouse_telephone' => '1234567890',
+                'is_active' => true,
+                'created_at' => '2023-01-01',
+                'updated_at' => '2023-01-02',
+            ],
+            [
+                'id' => 2,
+                'warehouse_name' => 'Warehouse B',
+                'warehouse_address' => 'Location B',
+                'warehouse_telephone' => '1234567890',
+                'is_active' => false,
+                'created_at' => '2023-02-01',
+                'updated_at' => '2023-04-01',
+            ],
+            [
+                'id' => 3,
+                'warehouse_name' => 'Warehouse C',
+                'warehouse_address' => 'Location C',
+                'warehouse_telephone' => '1234567890',
+                'is_active' => true,
+                'created_at' => '2023-01-06',
+                'updated_at' => '2023-01-04',
+            ],
+        ];
 
         $pdf = Pdf::loadView('warehouse.report', compact('warehouse'));
         return $pdf->stream('warehouse_report.pdf');
