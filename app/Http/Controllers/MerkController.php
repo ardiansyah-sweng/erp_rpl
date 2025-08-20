589eaf59572c58dc29888a62806abdd7fb709cb5
MPuguh <akunbarulol484@gmail.com>
2025-08-20 20:19:28 +0700

    view 2

--- app/Http/Controllers/MerkController.php
+++ app/Http/Controllers/MerkController.php
@@ -42,8 +42,7 @@ class MerkController extends Controller
     public function getMerkAll()
     {
         $merks = Merk::getAllMerk();
-
-        return response()->json($merks);
+        return view('merk.list', compact('merks'));
     }
 
      public function deleteMerk($id)
