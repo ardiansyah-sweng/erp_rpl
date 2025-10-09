8e120afae8b9186386b0780cee706bdeb58da3fb
Rifqi Chusaini <mrifqichusainis@gmail.com>
2025-10-09 18:40:50 +0700

    getProductByTypeTest Rifqi Chusaini

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -81,7 +81,7 @@ class Product extends Model
 
     public static function getProductByType($type)
     {
-         return self::where('product_type', $type)->get();
+         return self::where('type', $type)->get();
     }
     
     public static function updateProduct($id, array $data)//Sudah sesuai pada ERP RPL
