4aeaeca59b637e2cbb4d26490891b81318f2af4b
RezaMaulanaPutra <2300018253@webmail.uad.ac.id>
2026-01-05 18:45:37 +0700

    feat: implementasi fungsi addCategory dan feature testing

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -8,20 +8,17 @@ use App\Models\Category;
 
 class CategoryTest extends TestCase
 {
-    use RefreshDatabase; // Menghapus data setelah test selesai
+    use RefreshDatabase;
 
     /** @test */
 public function it_can_insert_category_to_database()
 {
-    // Ubah 'name' menjadi 'category' sesuai permintaan database Anda
     $categoryData = [
         'category' => 'Elektronik', 
     ];
 
-    // Jalankan aksi
     \App\Models\Category::create($categoryData);
 
-    // Verifikasi di database
     $this->assertDatabaseHas('categories', [
         'category' => 'Elektronik'
     ]);
