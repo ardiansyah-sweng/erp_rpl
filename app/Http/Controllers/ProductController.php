39902fc2929739d217dc73860d4dd36c9a4f880f
263 _ Muhammad Dewa Agupasa <2300018263@webmail.uad.ac.id>
2026-05-04 22:27:27 +0700

    fix Print pdf by catagory

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -134,7 +134,7 @@ class ProductController extends Controller
     {
         // Cari kategori berdasarkan ID
         $category = Category::find($id);
-
+        // Percabangan 
         if (!$category) {
             return response()->json([
                 'success' => false,
