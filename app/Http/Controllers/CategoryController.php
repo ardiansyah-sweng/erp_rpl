7157fcf3a5020e2c2b6f65ed2f7e5b5bae1f18ef
JaydenNoch <zaidanahmad005@gmail.com>
2026-05-07 13:38:31 +0700

    fix: final clean up pdf logic

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -79,15 +79,15 @@ class CategoryController extends Controller
     }
     //Search Category 
     public function searchCategory(Request $request)
-    {
-        $keyword = $request->input('q');
+{
+    $keyword = $request->input('search'); 
 
-        $category = Category::when($keyword, function ($query) use ($keyword) {
-            $query->where('category', 'like', '%' . $keyword . '%');
-        })->get();
+    $categories = Category::when($keyword, function ($query) use ($keyword) { 
+        $query->where('category', 'like', '%' . $keyword . '%');
+    })->paginate(10); 
 
-        return view('category.list', compact('category'));
-    }
+    return view('category.index', compact('categories', 'keyword')); 
+}
 
 
     // delete category
