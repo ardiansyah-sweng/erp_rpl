ab1be065bbd688e8c5c966bd7543d955e56dd61a
Fraankky <farisabdurrohman1414@gmail.com>
2026-05-11 19:54:26 +0700

    fix: delete duplicate method getProductByKeyword

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -101,21 +101,10 @@ class Product extends Model
         return $this->hasMany(Item::class, 'product_id', 'product_id');
     }
 
-    public static function getProductByKeyword($keyword)
-    {
-        return self::with('category')
-            ->where('product_id', 'like', "%{$keyword}%")
-            ->orWhere('product_name', 'like', "%{$keyword}%")
-            ->orWhere('product_type', 'like', "%{$keyword}%")
-            ->orWhere('product_description', 'like', "%{$keyword}%")
-            ->orderBy('created_at', 'desc')
-            ->paginate(10);
-    }
-
     public static function getProductByCategory($productCategory)
     {
         return self::with('category')
-            ->where('product_category', $productCategory)
+            ->where(ProductColumns::CATEGORY, $productCategory)
             ->orderBy('created_at', 'desc')
             ->paginate(10);
     }
