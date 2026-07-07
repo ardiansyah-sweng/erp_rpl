ba52142faba8cf342d58355910a933944b98760e
Adityananto <2300018210@webmail.uad.ac.id>
2026-07-07 11:34:09 +0700

    adityananto_deleteBillOfMaterial: tambah method delete Bill of Material di model

--- app/Http/Controllers/BillOfMaterialController.php
+++ app/Http/Controllers/BillOfMaterialController.php
@@ -33,7 +33,7 @@ class BillOfMaterialController extends Controller
     // Fungsi untuk menghapus Bill of Material berdasarkan id
     public function deleteBillOfMaterial($id)
     {
-        $deleted = BillOfMaterial::deleteBom($id);
+        $deleted = BillOfMaterial::deleteBillOfMaterial($id);
 
         if ($deleted) {
             return response()->json(['message' => 'Bill of Material deleted successfully.'], 200);
